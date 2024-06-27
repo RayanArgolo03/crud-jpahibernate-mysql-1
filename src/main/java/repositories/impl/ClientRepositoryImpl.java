5f83af6b06648db3d38b7b1e84a7fa11acea70d5
Rayan <rayanpetros2@gmail.com>
2024-06-27 19:24:33 -0300

    feat: add repos

---
+++ src/main/java/repositories/impl/ClientRepositoryImpl.java
@@ -0,0 +1,15 @@
+package repositories.impl;
+
+import domain.client.Client;
+import repositories.ClientRepository;
+
+import java.util.UUID;
+
+public final class ClientRepositoryImpl implements ClientRepository {
+
+    @Override
+    public Client save(Client client) {
+        //Implementar com banco de dados
+        return null;
+    }
+}
