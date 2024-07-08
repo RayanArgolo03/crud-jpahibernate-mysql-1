c95c3f967f508455c0839b31b765201c1940ce78
Rayan <rayanpetros2@gmail.com>
2024-07-08 09:45:03 -0300

    feat: adding JPAUTIL manager class

---
+++ src/main/java/utils/JPAUtils.java
@@ -0,0 +1,25 @@
+package utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import java.util.Objects;
+
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class JPAUtils {
+
+    static EntityManager MANAGER;
+
+    public static EntityManager getManager(final String unitPersistence) {
+
+        if (Objects.isNull(MANAGER)) {
+            MANAGER = Persistence.createEntityManagerFactory(unitPersistence).createEntityManager();
+        }
+
+        return MANAGER;
+    }
+}
