365f8b7e17b6c5fecb2ae2b58dd983741749ff6f
Rayan <rayanpetros2@gmail.com>
2024-07-18 09:37:55 -0300

    refactor: reducing code smell static class

---
+++ src/main/java/utils/FormatterCurrencyUtils.java
@@ -0,0 +1,17 @@
+package utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class FormatterCurrencyUtils {
+    private final static NumberFormat CURRENCY_FORMATTER = NumberFormat.getCurrencyInstance();
+
+    public static String formatCurrency(final BigDecimal value) {
+        return CURRENCY_FORMATTER.format(value);
+    }
+
+}
